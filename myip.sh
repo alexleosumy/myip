88b9321306eac7a4d8c4188f63b7fcbd85f7dab6
Oleksandr Nyzovyi <a.nizovoy@soft-industry.com>
2019-09-18 08:38:37 +0300

    add dependency

--- myip.sh
+++ myip.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+#need instaled links and whois
+
 ip=`links -dump http://checkip.dyndns.org/ | awk '{ print $4 }'`
-echo IP: $ip
 provider=`whois $ip | grep 'org-name' | awk '{$1=""}1'`
-echo Provider: $provider
+echo -e "IP: $ip\nProvider: $provider"
 
